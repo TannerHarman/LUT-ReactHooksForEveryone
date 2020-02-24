afe190601ec98f99d554812002fbf7946ee7b5eb
Tanner Harman <harmtan13@pop-os.localdomain>
2020-02-24 14:53:46 -0700

    Used useMemo hook to prevent a function from being called on Re-render.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useMemo } from 'react';
 import useTitleInput from './hooks//useTitleInput';
 import Toggle from './Toggle';
 import Counter from './Counter'
@@ -8,12 +8,23 @@ const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
 
+  const reverseWord = (word) => {
+    console.log('function called');
+    return word
+      .split('')
+      .reverse()
+      .join('');
+  }
+
+  const title = `Level Up Dishes`
+
+  const TitleReversed = useMemo(() => reverseWord(title), [title]);
+
   return (
 
     <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
+      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>{TitleReversed}</h1>
       <Toggle />
-      <Counter />
 
       <form onSubmit={(e) => {
         e.preventDefault();
