7513717599a7e8de9610624ca717c97db43ae8b2
Tanner Harman <harmtan13@pop-os.localdomain>
2020-02-26 10:27:59 -0700

    Added name to the useEffect() call to control with it re-rendered

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useMemo } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import useTitleInput from './hooks//useTitleInput';
 import Toggle from './Toggle';
 import Counter from './Counter'
@@ -8,22 +8,23 @@ const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
 
-  const reverseWord = (word) => {
-    console.log('function called');
-    return word
-      .split('')
-      .reverse()
-      .join('');
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    console.log('ran');
+    const res = await fetch('https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes');
+    const data = await res.json();
+
+    setDishes(data);
   }
 
-  const title = `Level Up Dishes`
-
-  const TitleReversed = useMemo(() => reverseWord(title), [title]);
+  useEffect(() => {
+    fetchDishes();
+  }, [name]);
 
   return (
-
     <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>{TitleReversed}</h1>
+      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
       <Toggle />
 
       <form onSubmit={(e) => {
@@ -39,6 +40,18 @@ const App = () => {
         <button>Submit</button>
       </form>
 
+      { dishes.map(dish => (
+        <article className="dish-card dish-card--withImage" >
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className="ingredients">
+            {dish.ingredients.map(ingredient => (
+              <span>{ingredient}</span>
+            ))}
+          </div>
+        </article>
+      ))}
+      
     </div>
   );
 };
