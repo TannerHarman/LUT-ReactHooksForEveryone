000414b0f53221d56805550102f770a33d8cae27
Tanner Harman <harmtan13@pop-os.localdomain>
2020-02-26 12:05:51 -0700

    Used 3rd party hook to replace api interface

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import useAbortableFetch from 'use-abortable-fetch';
 import useTitleInput from './hooks//useTitleInput';
 import Toggle from './Toggle';
 // import Counter from './Counter'
@@ -6,20 +7,10 @@ import Toggle from './Toggle';
 const App = () => {
 
   const [name, setName] = useTitleInput('');
+
   const ref = useRef();
-
-  const [dishes, setDishes] = useState([]);
-
-  const fetchDishes = async () => {
-    const res = await fetch('https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes');
-    const data = await res.json();
-
-    setDishes(data);
-  }
-
-  useEffect(() => {
-    fetchDishes();
-  }, []);
+  const { data, loading } = useAbortableFetch('https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes');
+  if(!data) return null;
 
   return (
     <div className="main-wrapper" ref={ref}>
@@ -39,7 +30,7 @@ const App = () => {
         <button>Submit</button>
       </form>
 
-      { dishes.map(dish => (
+      { data.map(dish => (
         <article className="dish-card dish-card--withImage" >
           <h3>{dish.name}</h3>
           <p>{dish.desc}</p>
